436f3bbee7ee6cee10b741267aa2dfb1da3c44b7
krloslopes <carloslopes23071980@gmail.com>
2025-04-23 20:28:39 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { RouterProvider,createBrowserRouter } from "react-router-dom"
+import LandingPage from "./pages/LandingPage/LandingPage"
+
+function App() {
+  
+const router = createBrowserRouter([
+  {path : '/', element: <LandingPage />},])
+  return <RouterProvider router={router} />
+}
+export default App
