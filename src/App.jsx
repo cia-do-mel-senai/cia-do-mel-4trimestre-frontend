9107a21f501796fd11babcfc8f8755b1f554cadc
Carlos Alexandre Lopes <146020709+krloslopes@users.noreply.github.com>
2025-05-27 21:30:09 -0300

    Inicio,criaçao do Catalogo criação do Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import ProductRegister from "./pages/ProductRegister/ProductRegister";
 import ProductEdit from "./pages/ProductEdit/ProductEdit";
+import Catalogo from "./pages/Catalogo/catalogo";
 
 function App() {
   const router = createBrowserRouter([
     { path: "/", element: <LandingPage /> },
     { path: "/product/new", element: <ProductRegister /> },
     { path: "/product/:id/edit", element: <ProductEdit /> },
+     { path: "/catalogo", element: < Catalogo/> },
   ]);
   return <RouterProvider router={router} />;
 }
---
+++ src/pages/Catalogo/Catalogo.jsx
@@ -0,0 +1,15 @@
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import "./Catalogo.css";
+
+export default function Catalogo() {
+  return (
+    <div className='catalogo-container'
+    >
+        <Header/>
+        
+        <Footer />
+
+    </div>
+  )
+}
