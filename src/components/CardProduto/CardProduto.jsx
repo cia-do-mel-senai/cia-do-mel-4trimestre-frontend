32fd719386a435dfff66cbbdd1c5a8e761e81699
Carlos Alexandre Lopes <146020709+krloslopes@users.noreply.github.com>
2025-05-29 21:50:03 -0300

    finalizaçao do catalogo e cardproduto

---
+++ src/components/CardProduto/CardProduto.jsx
@@ -0,0 +1,14 @@
+import { FaStar } from "react-icons/fa"
+import "./CardProduto.css"
+
+export default function CardProduto({nome,imagem,preco,nota}) {
+  return (
+    <div className="card-container"><img src={imagem} alt="" />
+    <div className="card-content"><p>{nome}</p><p>R$ {preco.toFixed(2)}</p><div><FaStar color="yellow"/><FaStar color="yellow"/><FaStar color="yellow"/><FaStar color="yellow"/><FaStar color="yellow"/></div><button>comprar</button></div>
+    </div>
+  )
+}
+
+
+
+
--- src/pages/Catalogo/Catalogo.jsx
+++ src/pages/Catalogo/Catalogo.jsx
@@ -1,15 +1,40 @@
+import CardProduto from "../../components/CardProduto/CardProduto";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import "./Catalogo.css";
 
 export default function Catalogo() {
-  return (
-    <div className='catalogo-container'
-    >
-        <Header/>
-        <div className="catalogo-content"></div>
-        <Footer />
+    return (
+        <div className='catalogo-container'
+        >
+            <Header />
+            <div className="catalogo-content">
+                <div className="catalogo-body">
+                    <div className="catalogo-filtros">
+                    <select name="" id="">
+                        <option value="">categorias</option>
+                        </select>
+                        <select name="" id="">
+                            <option value="">filtro</option>
+                        </select>
+                        </div>
+                        <div className="catalogo-produtos">
+            <CardProduto imagem={"https://www.fatosdesconhecidos.com.br/wp-content/uploads/2016/07/propriedades-do-mel-1.jpg"} nome={"Mel"} preco={19.999}/>
+            <CardProduto imagem={"https://www.fatosdesconhecidos.com.br/wp-content/uploads/2016/07/propriedades-do-mel-1.jpg"} nome={"Mel"} preco={19.999}/>
+            <CardProduto imagem={"https://www.fatosdesconhecidos.com.br/wp-content/uploads/2016/07/propriedades-do-mel-1.jpg"} nome={"Mel"} preco={19.999}/>
+            <CardProduto imagem={"https://www.fatosdesconhecidos.com.br/wp-content/uploads/2016/07/propriedades-do-mel-1.jpg"} nome={"Mel"} preco={19.999}/>
+            <CardProduto imagem={"https://www.fatosdesconhecidos.com.br/wp-content/uploads/2016/07/propriedades-do-mel-1.jpg"} nome={"Mel"} preco={19.999}/>
+            <CardProduto imagem={"https://www.fatosdesconhecidos.com.br/wp-content/uploads/2016/07/propriedades-do-mel-1.jpg"} nome={"Mel"} preco={19.999}/>
+            <CardProduto imagem={"https://www.fatosdesconhecidos.com.br/wp-content/uploads/2016/07/propriedades-do-mel-1.jpg"} nome={"Mel"} preco={19.999}/>
+            <CardProduto imagem={"https://www.fatosdesconhecidos.com.br/wp-content/uploads/2016/07/propriedades-do-mel-1.jpg"} nome={"Mel"} preco={19.999}/>
+            <CardProduto imagem={"https://www.fatosdesconhecidos.com.br/wp-content/uploads/2016/07/propriedades-do-mel-1.jpg"} nome={"Mel"} preco={19.999}/>
 
-    </div>
-  )
+                        </div>
+                </div>
+            </div>
+
+            <Footer />
+
+        </div>
+    )
 }
