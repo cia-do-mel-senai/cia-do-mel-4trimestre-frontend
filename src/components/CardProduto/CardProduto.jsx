419e4195e99db8a4374ea8aa4a2f26f770d3a12f
Victormalmanncunha <victorcunhasc@gmail.com>
2025-06-03 19:13:18 -0300

    Atualização do css do CardProduto e da página de catalogo

--- src/components/CardProduto/CardProduto.jsx
+++ src/components/CardProduto/CardProduto.jsx
@@ -1,14 +1,22 @@
-import { FaStar } from "react-icons/fa"
-import "./CardProduto.css"
+import { FaStar } from "react-icons/fa";
+import "./CardProduto.css";
 
-export default function CardProduto({nome,imagem,preco,nota}) {
+export default function CardProduto({ nome, imagem, preco, nota }) {
   return (
-    <div className="card-container"><img src={imagem} alt="" />
-    <div className="card-content"><p>{nome}</p><p>R$ {preco.toFixed(2)}</p><div><FaStar color="yellow"/><FaStar color="yellow"/><FaStar color="yellow"/><FaStar color="yellow"/><FaStar color="yellow"/></div><button>comprar</button></div>
+    <div className="card-container">
+      <img src={imagem} alt="" />
+      <div className="card-content">
+        <p>{nome}</p>
+        <p>R$ {preco.toFixed(2)}</p>
+        <div>
+          <FaStar size={24} color="yellow" />
+          <FaStar size={24} color="yellow" />
+          <FaStar size={24} color="yellow" />
+          <FaStar size={24} color="yellow" />
+          <FaStar size={24} color="yellow" />
+        </div>
+        <button>Comprar</button>
+      </div>
     </div>
-  )
+  );
 }
-
-
-
-
